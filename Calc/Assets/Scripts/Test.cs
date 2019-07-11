9599059bd4ffd8e459981427fe713cf331c862ee
長谷川　光希 <s17gd04@201MAC01.local>
2019-07-11 11:37:36 +0900

    テスト環境作成

---
+++ Calc/Assets/Scripts/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        // テスト用配列
+        Analyze analyze = new Analyze(new List<Calc.IB>{Calc.IB.IB1});
+        float result = 0;
+        bool success = analyze.GetResult(ref result);
+        Debug.Log(result);
+    }
+}
